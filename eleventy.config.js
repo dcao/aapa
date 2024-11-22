c2ef1a055d6a0b4d0c700975e5d83ee4ae47d4f8
David Cao <david@cao.sh>
2024-11-21 17:51:39 -0800

    layout stuff!

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,17 @@
 import { EleventyHtmlBasePlugin } from "@11ty/eleventy";
+import { parse } from "csv-parse/sync";
+import { readFileSync } from "fs";
+
+function readMedia() {
+    const input = readFileSync("./website/_data/media.csv");
+    const records = parse(input, {
+        columns: true,
+        skip_empty_lines: true,
+    });
+    console.log(`${records.length} records found.`);
+    return records;
+}
+
 
 export default async function(eleventyConfig) {
     eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
@@ -8,7 +21,24 @@ export default async function(eleventyConfig) {
 
     // Extra files
     eleventyConfig.addPassthroughCopy('website/assets');
-
     eleventyConfig.setLayoutsDirectory("_layouts");
+
+    eleventyConfig.addPassthroughCopy('website/media/files');
+    eleventyConfig.addDataExtension("csv", (contents) => {
+        const records = parse(contents, {
+            columns: true,
+            skip_empty_lines: true,
+        });
+        return records;
+    });
+    const media = readMedia();
+
+    for (const mediaItem of media) {
+        const path = `media/${mediaItem.filename}.liquid`;
+        eleventyConfig.addTemplate(path, mediaItem.description, {
+            layout: "media.liquid",
+            filename: mediaItem.filename,
+        });
+    }
 };
 
